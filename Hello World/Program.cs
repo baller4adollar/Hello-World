64e793977468dc5bbcf0eb96bf040ce9e0333cd5
Nikhil Patel <nikhil8@usf.edu>
2020-09-02 19:57:48 -0700

    Version 2 - Hello World

--- Hello World/Program.cs
+++ Hello World/Program.cs
@@ -6,7 +6,10 @@ namespace Hello_World
     {
         static void Main(string[] args)
         {
+            // Pause to display Hello World!
+            Console.WriteLine(" Press any key to continue...");
             Console.WriteLine("Hello World!");
+            Console.ReadKey(true);
         }
     }
 }
